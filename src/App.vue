506cff1d86e308a542e7357acdd6be5e5ebd7b1c
jasonyeske <jasonyeske@sasktel.net>
2022-03-17 14:34:19 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader :title="pageHeader"  />
+  
+ 
+  <TheFooter />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import TheHeader from './components/TheHeader.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+import TheFooter from './components/TheFooter.vue';
+const pageHeader = 'home page!';
+
+
+
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+<header>
+<h1> {{props.title}} </h1>
+<p>{{props.description}}</p>
+</header>
+
+
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+const props =  defineProps ({
+    title: {
+        type: String,
+        default: 'Default Text'
+    },
+    description: {
+        type: String,
+        default:
+            'A Description'
+    }
+});
+
+   
+
+
+    
+
+</script>
